462f6ba9d95d3f52aed0620238012c0b16177e6f
Evan <dunneev@gmail.com>
2021-02-01 02:17:15 -0500

    "Open file" logic moved to controller

--- Controller.py
+++ Controller.py
@@ -1,6 +1,9 @@
 from Model import Model
 from View import View
 from Observer import Observer, Observable
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import csv
+
 
 
 class Controller:
@@ -18,9 +21,29 @@ class Controller:
         
 
     def open_file(self):
-        self.view.open_file()
+        """Open a file for editing & solving."""
+        filepath = askopenfilename(
+            filetypes=[("Sausage Files", "*.csv"), ("All Files", "*.*")]
+        )
+
+        if not filepath:
+            return
+
+        with open(filepath, newline="") as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                self.model.addGridRow(row)
+
+        self.view.window.title(f"Sausage Solver - {filepath}")
+        self.gridChanged(self.model.grid)
 
     
     def save_file(self):
         self.view.save_file()
+
+
+
+    def gridChanged(self, grid):
+        self.view.setGridView(grid)
 
