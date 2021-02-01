4ab1e90f86da364fb7f6d35ddd89a871c3a48740
Evan <dunneev@gmail.com>
2021-02-01 02:18:12 -0500

    GUI grid logic implemented w centered text

--- View.py
+++ View.py
@@ -1,78 +1,70 @@
 import tkinter as tk
-import csv
-from tkinter import Canvas
-from tkinter.filedialog import askopenfilename, asksaveasfilename
-# from tkinter.constants import *
 
 
 class View:
-    def open_file(self):
-        """Open a file for editing."""
-        filepath = askopenfilename(
-            filetypes=[("Sausage Files", "*.csv"), ("All Files", "*.*")]
-        )
 
-        if not filepath:
-            return
+    def setGridView(self, grid):
 
-        with open(filepath, newline="") as file:
-            self.window.title(f"Sausage Solver - {filepath}")
-            reader = csv.reader(file)
+        r = 0
+        for row in grid:
+            self.fr_map.columnconfigure(r, weight=1)
+            self.fr_map.rowconfigure(r, weight=1)
+            c = 0
+            for col in row:
+                frame = tk.Frame(
+                    master=self.fr_map,
+                    relief=tk.RAISED,
+                    borderwidth=1
+                )
 
-            r = 0
-            for row in reader:
-                self.fr_map.columnconfigure(r, weight=1)
-                self.fr_map.rowconfigure(r, weight=1)
-                c = 0
-                for col in row:
-                    frame = tk.Frame(
-                        master=self.fr_map,
-                        relief=tk.RAISED,
-                        borderwidth=1
-                    )
+                tk.Canvas(master=frame)
 
-                    # Set propagate to false to prevent frames from wrapping content
-                    frame.propagate(0)
-                    frame.grid(row=r, column=c, sticky="nsew")
+                # Set propagate to false to prevent frames from wrapping content
+                frame.propagate(0)
+                frame.grid(row=r, column=c, sticky="nsew")
 
-                    canvas = Canvas(master=frame)
-                    canvas.create_text(
-                        0,
-                        0,
-                        text=col,
-                        font=12
-                    )
+                # label = tk.Label(master=frame, text=col)
+                # label.pack()
 
-                    canvas.pack()
+                c += 1
+            r += 1
+        self.updateGridTextPosition()
 
-                    # label = tk.Label(master=frame, text=col)
-                    # label.pack()
 
-                    c += 1
-                r += 1
+    def updateGridTextPosition(self):
+        """ reposition text in frame map frames """
+        
+        self.fr_map.update_idletasks()
+        for frame in self.fr_map.winfo_children():
+            for canvas in frame.winfo_children():
+                canvas.delete('all')
+                canvas.create_text(frame.winfo_width()/2,
+                               frame.winfo_height()/2, 
+                               text='hey')
+                canvas.pack()      
 
-    def save_file(self):
-        """Save the current file as a new file."""
+    # def save_file(self):
+    #     """Save the current file as a new file."""
 
-        filepath = asksaveasfilename(
+    #     filepath = asksaveasfilename(
 
-            defaultextension="txt",
-            filetypes=[("Sausage Files", "*.csv"), ("All Files", "*.*")],
-        )
+    #         defaultextension="txt",
+    #         filetypes=[("Sausage Files", "*.csv"), ("All Files", "*.*")],
+    #     )
 
-        if not filepath:
-            return
+    #     if not filepath:
+    #         return
 
-        with open(filepath, "w") as output_file:
-            text = self.txt_edit.get("1.0", tk.END)
-            output_file.write(text)
+    #     with open(filepath, "w") as output_file:
+    #         text = self.txt_edit.get("1.0", tk.END)
+    #         output_file.write(text)
 
-        self.window.title(f"Sausage Solver - {filepath}")
+    #     self.window.title(f"Sausage Solver - {filepath}")
 
-    def on_configure(self, event):
+    def onConfigure(self, event):
         print("onConfigure")
+        self.updateGridTextPosition()
         # print(str(fr_map.winfo_children()))
-
 
     def __init__(self, window):
         self.window = window
@@ -81,7 +73,6 @@ class View:
         self.fr_buttons = tk.Frame(window)
         window.title("Sausage Solver")
 
-
         # One row, which grows proportionally
         self.window.rowconfigure(0, weight=1, minsize=200)
 
@@ -89,8 +80,6 @@ class View:
         # and this one, which will grow and shrink proportionally
         self.window.columnconfigure(1, weight=3, minsize=200)
 
-        
-        
         self.btn_open = tk.Button(self.fr_buttons, text="Open")
         self.btn_save = tk.Button(self.fr_buttons, text="Save As...")
 
@@ -101,5 +90,4 @@ class View:
         # txt_edit.grid(row=0, column=1, sticky="nsew")
         self.fr_map.grid(row=0, column=1, sticky="nsew")
 
-        self.fr_map.bind('<Configure>', self.on_configure(self))
-
+        self.fr_map.bind(sequence='<Configure>', func=self.onConfigure)
