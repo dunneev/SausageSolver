87c8426cd7aacdffba83d4b631450e54ef8fd895
Evan <dunneev@gmail.com>
2021-02-01 02:17:43 -0500

    Grid variable added to model

--- Model.py
+++ Model.py
@@ -3,5 +3,11 @@ from Observer import Observable
 class Model:
     def __init__(self):
         self.events = Observable([])
+        self.grid = []
+
+    def addGridRow(self, row):
+        self.grid.append(row)
+
+
 
         
